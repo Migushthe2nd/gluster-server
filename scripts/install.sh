d79d1d3d1fb4a14b1383b707565b756d53c29c41
Migush <Migushthe2nd@users.noreply.github.com>
2023-04-03 21:31:36 +0200

    Replace ubuntu repo with debian

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# install more packages
-apt-get -y update \
-    && apt-get -y install software-properties-common
-
 # add latest glusterfs repo
-add-apt-repository -y ppa:gluster/glusterfs-11
+wget -O - https://download.gluster.org/pub/gluster/glusterfs/9/rsa.pub | apt-key add -
+DEBID=$(grep 'VERSION_ID=' /etc/os-release | cut -d '=' -f 2 | tr -d '"')
+DEBVER=$(grep 'VERSION=' /etc/os-release | grep -Eo '[a-z]+')
+DEBARCH=$(dpkg --print-architecture)
+echo deb https://download.gluster.org/pub/gluster/glusterfs/LATEST/Debian/${DEBID}/${DEBARCH}/apt ${DEBVER} main > /etc/apt/sources.list.d/gluster.list
+apt update
 
 # install more packages
-apt-get -y install attr psmisc glusterfs-server
+apt-get -y glusterfs-server install attr psmisc
 
 # Clean up
 apt-get -y autoremove \
