fe561b3b9df0673af76ecb4b847cba4ab70cbf2b
Migush <Migushthe2nd@users.noreply.github.com>
2023-04-03 21:04:35 +0200

    Add missing software-properties-common

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# install more packages
+apt-get -y update \
+    && apt-get -y install software-properties-common
+
 # add latest glusterfs repo
 add-apt-repository -y ppa:gluster/glusterfs-11
 
 # install more packages
-apt-get -y update \
-    && apt-get -y install attr psmisc glusterfs-server
+apt-get -y install attr psmisc glusterfs-server
 
 # Clean up
 apt-get -y autoremove \
